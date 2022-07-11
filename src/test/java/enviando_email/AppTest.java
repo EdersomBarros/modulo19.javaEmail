dbd74bb5588711ac117a2e5f8ea1471bced35956
Barros <edersombarros@hotmail.com>
2022-07-11 15:50:05 -0400

    Configurando as propriedades

--- src/test/java/enviando_email/AppTest.java
+++ src/test/java/enviando_email/AppTest.java
@@ -1,7 +1,10 @@
 package enviando_email;
 
+import java.util.Properties;
+
 /**
  * Unit test for simple App.
+ * yannqnlhkohmgnjd
  */
 public class AppTest{
 	
@@ -9,6 +12,13 @@ public class AppTest{
 	public void testeEmail() {
 		/*olhe as configurações smtp do email*/
 		
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true");/*Autorização*/
+		properties.put("mail.smtp.starttls", "true");/*Autenticação*/
+		properties.put("mail.smtp.host", "smtp.gmail.com");/*Servidor gmail*/
+		properties.put("mail.smtp.port", "465");/*portal do servidor*/
+		properties.put("mail.smtp.socketFactory.port", "465");/*Expecificar a porta a ser conectada pelo socket*/
+		properties.put("mail.smatp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");/*Classe socket de conexão ao smtp*/
 		
 	}
 	
