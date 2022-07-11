7945a584a8754ab1d2bf610d008b590cb28f8d3c
Barros <edersombarros@hotmail.com>
2022-07-11 15:05:32 -0400

    primeiro commit

---
+++ src/test/java/enviando_email/AppTest.java
@@ -0,0 +1,16 @@
+package enviando_email;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+	
+	@org.junit.Test
+	public void testeEmail() {
+		/*olhe as configurações smtp do email*/
+		
+		
+	}
+	
+    
+}
